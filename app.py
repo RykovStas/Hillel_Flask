2384215543badf3873a610095b178d5392b648b8
Stas <stasyan_2003@icloud.com>
2023-02-22 17:45:32 +0200

    second task with some problems

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+import flask
+from flask import Flask, url_for, render_template, request
+from faker import Faker
+
 
 app = Flask(__name__)
-
+fake = Faker()
 
 @app.route('/requirements/')
 def open_file():
     with open('requirements.txt', 'r') as file:
         text = file.read().splitlines()
     return text
+
+
+@app.route('/generate-users/')
+def generate_users():
+    user_data = []
+    count = int(request.args.get('count', default=100))
+    return render_template('users.html', count= count)
 
