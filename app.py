26b4dcf8164dbcd9a21865f2496696a3bdc9ef36
Stas <stasyan_2003@icloud.com>
2023-02-22 18:20:24 +0200

    complete third task

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 from flask import Flask, url_for, render_template, request
 from faker import Faker
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -18,4 +19,13 @@ def generate_users():
     user_data = []
     count = int(request.args.get('count', default=100))
     return render_template('users.html', count= count)
+
+
+@app.route('/mean/')
+
+def mean():
+    dataset = pd.read_csv('hw.csv')
+    average = dataset.mean()
+    return f"Avarage height {round(average.values[1]*2.54)}sm, Avarage weight {round(average.values[2]/2.2)}kg"
+
 
